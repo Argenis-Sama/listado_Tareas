57377e9caf36ea7530d98b81673b3fd3e63d4555
Argenis Samaniego <samaniegoargenis@hotmail.com>
2024-10-01 22:41:17 -0500

    quinta subida

--- php/Constantes.php
+++
@@ -1,5 +0,0 @@
-<?php
-const servidor = "localhost";  // Nombre o IP del servidor
-const usuario = "argenis";        // Usuario de la base de datos
-const clave = "100%Samaniego";              // Contraseña del usuario
-const base_datos = "task_manager";  // Nombre de la base de datos
